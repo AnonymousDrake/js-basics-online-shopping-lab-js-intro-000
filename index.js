d4de0bc8255be7ae603883467490129e9019e7b4
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 13:06:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,23 +8,3 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
-function addToCart(item) {
- // write your code here
-}
-
-function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
